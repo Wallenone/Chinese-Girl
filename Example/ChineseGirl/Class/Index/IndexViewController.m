ad883655630e76da7439b1c4e8996a6ce27d8ad2
wallen <wanjie@huizhaofang.com>
2017-10-11 11:04:50 +0800

    修改首页

--- Example/ChineseGirl/Class/Index/IndexViewController.m
+++ Example/ChineseGirl/Class/Index/IndexViewController.m
@@ -12,7 +12,7 @@
 #import "IndexCollectionView.h"
 #import "MyIndexViewController.h"
 @interface IndexViewController ()<BHInfiniteScrollViewDelegate>
-@property (nonatomic, strong) BHInfiniteScrollView* infinitePageView;
+@property (nonatomic, strong) UIScrollView *scrollViewPointView;
 @property (nonatomic, strong) IndexCollectionView *indexCollectionView;
 @end
 
@@ -27,7 +27,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.automaticallyAdjustsScrollViewInsets = NO;
-    
+    [self addSubViews];
     [self setHeaderView];
     [self setScrollViewPoint];
     [self setTableView];
@@ -37,43 +37,31 @@
     [self setUpNavWithTitle:@"onLine" leftIcon:nil rightIcon:nil leftTitle:nil rightTitle:nil delegate:nil];
 }
 
+-(void)addSubViews{
+    [self.view addSubview:self.scrollViewPointView];
+}
+
 -(void)setScrollViewPoint{
-    NSArray* urlsArray = @[
-                           @"http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1602/26/c0/18646722_1456498424671_800x600.jpg",
-                           @"http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1602/26/c0/18646649_1456498410838_800x600.jpg",
-                           @"http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1602/26/c0/18646706_1456498430419_800x600.jpg",
-                           @"http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1602/26/c0/18646723_1456498427059_800x600.jpg",
-                           @"http://img.pconline.com.cn/images/upload/upc/tx/wallpaper/1602/26/c0/18646705_1456498422529_800x600.jpg"
-                           ];
-    
-    
-    NSArray* titlesArray = @[@"欢迎使用BHInfiniteScrollView无限轮播图",
-                             @"如果你在使用过程中遇到什么疑问",
-                             @"可以添加QQ群：206177395",
-                             @"我会及时修复bug",
-                             @"为你解答问题",
-                             ];
-    
-    
-    self.infinitePageView = [BHInfiniteScrollView
-                                               infiniteScrollViewWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.frame), 100) Delegate:self ImagesArray:urlsArray];
-    self.infinitePageView.backgroundColor=[UIColor redColor];
-    self.infinitePageView.titlesArray = titlesArray;
-    self.infinitePageView.dotSize = 10;
-    self.infinitePageView.pageControlAlignmentOffset = CGSizeMake(0, 20);
-    self.infinitePageView.titleView.textColor = [UIColor colorWithRed:255/255 green:255/255 blue:255/255 alpha:0.24];
-    self.infinitePageView.dotColor = [UIColor redColor];
-    self.infinitePageView.titleView.margin = 30;
-    self.infinitePageView.titleView.hidden = YES;
-    self.infinitePageView.scrollTimeInterval = 2;
-    self.infinitePageView.autoScrollToNextPage = YES;
-    self.infinitePageView.delegate = self;
-    [self.view addSubview:self.infinitePageView];
+    for (int i=0; i<7; i++) {
+        UIImageView *imgView=[[UIImageView alloc] initWithFrame:CGRectMake(10*SCREEN_RADIO+i*(15+60)*SCREEN_RADIO, 10*SCREEN_RADIO, 60*SCREEN_RADIO, 60*SCREEN_RADIO)];
+        imgView.layer.cornerRadius=30*SCREEN_RADIO;
+        imgView.image=[UIImage imageNamed:@"Avatar"];
+        imgView.layer.borderWidth=2;
+        imgView.layer.borderColor=[UIColor getColor:@"FF7D44"].CGColor;
+        [self.scrollViewPointView addSubview:imgView];
+        
+        UILabel *nickName=[[UILabel alloc] initWithFrame:CGRectMake(10*SCREEN_RADIO+i*(15+60)*SCREEN_RADIO, CGRectGetMaxY(imgView.frame)+5*SCREEN_RADIO, 60*SCREEN_RADIO, 12*SCREEN_RADIO)];
+        nickName.text=[NSString stringWithFormat:@"Avatar%d-%d",i,i];
+        nickName.textColor=[UIColor getColor:@"333333"];
+        nickName.textAlignment=NSTextAlignmentCenter;
+        nickName.font=[UIFont systemFontOfSize:12*SCREEN_RADIO];
+        [self.scrollViewPointView addSubview:nickName];
+    }
     
 }
 
 -(void)setTableView{
-    self.indexCollectionView=[[IndexCollectionView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.infinitePageView.frame), self.view.frame.size.width, self.view.frame.size.height) onCellSelected:^(NSIndexPath  *indexPath) {
+    self.indexCollectionView=[[IndexCollectionView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.scrollViewPointView.frame), self.view.frame.size.width, self.view.frame.size.height) onCellSelected:^(NSIndexPath  *indexPath) {
         MyIndexViewController *index=[[MyIndexViewController alloc] init];
         [self.navigationController pushViewController:index animated:NO];
     }];
@@ -94,6 +82,13 @@
     //NSLog(@"did select item at index %ld", index);
 }
 
-
+-(UIScrollView *)scrollViewPointView{
+    if (!_scrollViewPointView) {
+        _scrollViewPointView=[[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, screen_width, 90*SCREEN_RADIO)];
+        _scrollViewPointView.contentSize=CGSizeMake(screen_width*2, 90*SCREEN_RADIO);
+    }
+    
+    return _scrollViewPointView;
+}
 
 @end
