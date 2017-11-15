f16eee41f8ad8816f976c5d0efd3ba5f4b0c3ba7
wallen <wanjie@huizhaofang.com>
2017-11-15 10:36:26 +0800

    增加通讯录

---
+++ Example/ChineseGirl/Class/Friends/CGFriendsViewcontroller.h
@@ -0,0 +1,15 @@
+//
+//  CGFriendsViewcontroller.h
+//  PinyinIndexTableview
+//
+//  Created by luo.h on 15-5-14.
+//  Copyright (c) 2015年 sibu.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGFriendsViewcontroller : UIViewController
+
+
+@end
+
