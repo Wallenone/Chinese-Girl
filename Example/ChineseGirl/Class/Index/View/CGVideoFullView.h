0415340a5ed9e81c01ecfd81c5dac45d8a9733ac
Wallen <wallen@Wallens-MacBook-Air.local>
2017-11-19 00:16:11 +0800

    增加首页视频功能

---
+++ Example/ChineseGirl/Class/Index/View/CGVideoFullView.h
@@ -0,0 +1,13 @@
+//
+//  CGVideoFullView.h
+//  ChineseGirl_Example
+//
+//  Created by Wallen on 2017/11/18.
+//  Copyright © 2017年 wanjiehuizhaofang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CGVideoFullView : UICollectionViewCell
+
+@end
---
+++ Example/ChineseGirl/Class/Index/View/CGVideoFullView.m
@@ -0,0 +1,13 @@
+//
+//  CGVideoFullView.m
+//  ChineseGirl_Example
+//
+//  Created by Wallen on 2017/11/18.
+//  Copyright © 2017年 wanjiehuizhaofang. All rights reserved.
+//
+
+#import "CGVideoFullView.h"
+
+@implementation CGVideoFullView
+
+@end
