bbaaf173043944a6829b61571678225e660fadb2
wallen <wanjie@huizhaofang.com>
2018-01-19 17:13:10 +0800

    优化

--- Example/ChineseGirl/CommonClass/CGGlobalTimerNode.m
+++ Example/ChineseGirl/CommonClass/CGGlobalTimerNode.m
@@ -15,7 +15,7 @@
 
 +(void)addTalkData{
     [[ZXCCycleTimer shareInstance] addCountDownWithTimeInterval:10 endBlock:^() {
-        NSLog(@"十秒钟后执行了这个时间，并且添加系统聊天");
+        //NSLog(@"十秒钟后执行了这个时间，并且添加系统聊天");
        // [CGUserInfo getTableRondomNewsUser];
     }];
 }
